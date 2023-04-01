b7b1eb52ae9861c785308fcde698a8d5d9a8e086
Bonnaroo <43123441+jakebonney10@users.noreply.github.com>
2023-03-31 23:22:11 -0400

    Added motion update for particles.

--- lander_pf.m
+++ lander_pf.m
@@ -1,14 +1,15 @@
 % lander_pf script
 % Bonney and Parisi
 
-% GOAL: Initialize particles for particle filter localization.
+% GOAL: Initialize particles for particle filter localization and perform 
+% motion update step for each particle.
 
 clc; clear all; 
 
 % Initial State
 ship_x = 0; % Ship latitude at launch
 ship_y = 0; % Ship longitude at launch
-position_std_dev = 5; % (m)
+position_std_dev = 1; % (m)
 velocity_std_dev = 0.1; % (m/s)
 avg_descent_veloc = 0.5; % (m/s)
 descent_std_dev = 0.1; % (m/s)
@@ -21,7 +22,7 @@ DeltaT = 0.2;
 state = struct('x', [], 'y', [], 'z', [], 'u', [], 'v', [], 'w', [], 'mode', [], 'bottom_time', []);
 
 % Initialize particles
-num_particles = 1000;
+num_particles = 10;
 initial_x = ship_x + position_std_dev * randn(num_particles, 1);
 initial_y = ship_y + position_std_dev * randn(num_particles, 1);
 initial_z = 5 + position_std_dev * randn(num_particles, 1);
@@ -40,8 +41,9 @@ state.w = initial_w;
 state.mode = initial_mode;
 state.bottom_time = initial_bottom_time;
 
-% Plot particles
+% Plot initial particle state
 plot3(state.x,state.y,state.z,'b.')
+set(gca, 'ZDir', 'reverse');
 axis equal
 hold on
 
@@ -51,18 +53,30 @@ motion_noise = 0.1; % noise parameter for velocity and acceleration
 mode_probabilities = [0.5, 0.1, 0.1, 0.3]; % probabilities of mode transitions
 
 for t=0:DeltaT:Tmax
-    
-    % motion model update
-    state.x = state.x + normrnd(0, position_std_dev);
-    state.y = state.y + normrnd(0, position_std_dev);
-    state.z = state.z + state.w*DeltaT + normrnd(0, descent_std_dev);
-    state.u = state.u + normrnd(0, velocity_std_dev);
-    state.v = state.v + normrnd(0, velocity_std_dev);
-    state.w = avg_descent_veloc + normrnd(0, velocity_std_dev);
-    state.mode = state.mode;
-    state.bottom_time = state.bottom_time;
+
+    % motion model update for each particle
+    for i=1:num_particles
+
+        state.x(i) = state.x(i) + normrnd(0, position_std_dev);
+        state.y(i) = state.y(i) + normrnd(0, position_std_dev);
+        state.z(i) = state.z(i) + state.w(i)*DeltaT + normrnd(0, descent_std_dev);
+        state.u(i) = state.u(i) + normrnd(0, velocity_std_dev);
+        state.v(i) = state.v(i) + normrnd(0, velocity_std_dev);
+        state.w(i) = avg_descent_veloc + normrnd(0, velocity_std_dev);
+        state.mode(i) = state.mode(i);
+        state.bottom_time(i) = state.bottom_time(i);
+
+    end
+
     plot3(state.x,state.y,state.z,'r.')
+
+    %pause(0.1)
 
 end
 
-set(gca, 'ZDir', 'reverse');
+%%%%% OUTPUTS
+disp('simulation ended!')
+final_particle_pose_x = mean(state.x);
+final_particle_pose_y = mean(state.y);
+final_particle_pose_z = mean(state.z);
+fprintf('The estimated position is x = %.2f, y = %.2f\n, z = %.2f\n',final_particle_pose_x, final_particle_pose_y, final_particle_pose_z)
