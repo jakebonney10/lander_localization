4551bc354698b7747cbf97ae1a4c1b42003121dd
Phil Parisi <philparisi@uri.edu>
2023-04-18 11:31:31 -0400

    prob density added
    sweeeeeet

--- lander_pf.m
+++ lander_pf.m
@@ -202,6 +202,7 @@ end
 
 %%
 %%%%% OUTPUTS
+clc
 
 disp('simulation ended!')
 final_particle_pose_x = mean(state.x);
@@ -213,7 +214,7 @@ fprintf('The estimated position is x = %.2f, y = %.2f, z = %.2f\n.',final_partic
 csv_fn = 'lander_iridium_sept2018.csv';
 [local_x, local_y, surface_t] = ground_truth(csv_fn, p);
 distance = sqrt((local_x - final_particle_pose_x)^2 + (local_y - final_particle_pose_y)^2);
-fprintf(' The distance between the ground truth and estimate is %.2f meters',distance)
+fprintf(' The distance between the ground truth and estimate is %.2f meters\n',distance)
 
 
 % Plot final point cloud top down view
@@ -228,7 +229,52 @@ axis equal
 
 %%%%% implement density solution here
 
+% generate meshgrid
+grid_size = 50;
+n = length(state.x);
 
+x_edges = linspace(min(state.x),max(state.x),grid_size);
+y_edges = linspace(min(state.y),max(state.y),grid_size);
+[X, Y] = meshgrid(x_edges, y_edges);
+
+% Count the number of points in each meshgrid cell
+counts = histcounts2(state.x, state.y, x_edges, y_edges);
+        % gives us values between our edges
+
+% create new probability map with shifted coords (x,y,counts)
+X_diff = abs(X(1,1) - X(1,2))/2;  % get shift
+X_new = X + X_diff;               % shift X
+X_new = X_new(1:end-1,1:end-1);   % fix size
+prob_map.X = X_new;
+
+Y_diff = abs(Y(1,1) - Y(2,1))/2;  % get shift
+Y_new = Y + Y_diff;               % shift Y
+Y_new = Y_new(1:end-1,1:end-1);   % fix size
+prob_map.Y = Y_new;
+
+prob_map.Z = counts/n;  % 'probability' in z
+
+% surface plot
+figure()
+surf(prob_map.X, prob_map.Y, prob_map.Z)
+xlabel('x'),ylabel('y'),zlabel('probability')
+
+% heat map
+figure()
+imagesc(prob_map.X(1,:), prob_map.Y(:,1)', prob_map.Z)
+set(gca, 'YDir', 'normal');
+xlabel('x'),ylabel('y')
+cb = colorbar;
+cb.Label.String = 'probability';
+
+% output the peak
+[prob_map.max_z, max_idx] = max(prob_map.Z(:));
+[max_row, max_col] = ind2sub(size(counts), max_idx);
+
+prob_map.max_y = prob_map.Y(max_row,1);
+prob_map.max_x = prob_map.X(1,max_col);
+
+fprintf('Max x is %0.3f, max y is %0.3f\n', prob_map.max_x, prob_map.max_y)
 
 
 
